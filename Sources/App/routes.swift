cf22ff627417144f424a19abe9c7ff9008356cae
akarsh <sakarshkumar@gmail.com>
2018-10-14 16:18:59 +0200

    removed default route and assigned view route as default route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,8 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
     // welcome.leaf route
-    router.get("view") {
+    router.get {
         req -> Future<View> in
         return try req.view().render("welcome")
     }
