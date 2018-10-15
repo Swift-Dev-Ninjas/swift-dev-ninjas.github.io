14f164a3fddf46b7edfe6697c44425f83d60f6f3
Akarsh Seggemu <sakarshkumar@gmail.com>
2018-10-15 17:36:00 +0200

    Removed sql package and reordered the css files & image file
    * removed the sql package as we are not using the database in this application.
    * reordered the css files & image file.

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         // 🍃 Leaf
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
